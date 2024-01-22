c7f139493b2d72c3adc5e2982df9c57f37fab6d7
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-01-21 21:59:35 -0700

    Add CreateRoomPage component with form controls and layout

--- juke_box_join/frontend/src/components/CreateRoomPage.js
+++ juke_box_join/frontend/src/components/CreateRoomPage.js
@@ -1,7 +1,65 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 const CreateRoomPage = () => {
-  return <h1>CreateRoomPage</h1>;
+  const defaultVotes = 2;
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} sx={{ textAlign: "center" }}>
+        <Typography component="h4" variant="h4">
+          Create A Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sx={{ textAlign: "center" }}>
+        <FormControl component="fieldset">
+          <FormHelperText sx={{ textAlign: "center" }}>
+            Guest Control of Playback State
+          </FormHelperText>
+          <RadioGroup row defaultValue="true">
+            <FormControlLabel
+              value="true"
+              control={<Radio color="primary" />}
+              label="Play/Pause"
+              labelPlacement="bottom"
+            />
+            <FormControlLabel
+              value="false"
+              control={<Radio color="secondary" />}
+              label="No Control"
+              labelPlacement="bottom"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} sx={{ textAlign: "center" }}>
+        <FormControl>
+          <TextField required={true} type='number' defaultValue={defaultVotes} inputProps={{
+            min: 1,
+            style: {textAlign: 'center'}
+          }} />
+          <FormHelperText sx={{ textAlign: "center" }}>
+            Votes Required to Skip Song
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} sx={{ textAlign: "center" }}>
+        <Button color='primary' variant='contained' >Create A Room</Button>
+      </Grid>
+      <Grid item xs={12} sx={{ textAlign: "center" }}>
+        <Button color='secondary' variant='contained' to='/' component={Link} >Back</Button>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default CreateRoomPage;
