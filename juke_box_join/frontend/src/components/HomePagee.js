cfe303a32caed94c6c888dddb6d5c1af20c0e773
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-01-27 16:00:35 -0700

    implament homepage component layout complete

--- juke_box_join/frontend/src/components/HomePagee.js
+++ juke_box_join/frontend/src/components/HomePagee.js
@@ -1,23 +1,41 @@
-import React from "react";
-import { Routes, Route, Link, Redirect, useRouteMatch } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import PageNotFound from "./PageNotFound";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
 
 const HomePagee = () => {
-  // let match = useRouteMatch();
-
   return (
     <Routes>
-      {/* Routes  */}
+      {/* Home page route */}
+      <Route
+        path="/"
+        element={
+          <Grid container spacing={3}>
+            <Grid item xs={12} sx={{ textAlign: "center" }}>
+              <Typography variant="h3" component="h3">
+                Jukebox Join
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sx={{ textAlign: "center" }}>
+              <ButtonGroup disableElevation variant="contained">
+                <Button color="primary" to="/join" component={Link}>
+                  Join Room
+                </Button>
+                <Button color="secondary" to="/create" component={Link}>
+                  Create Room
+                </Button>
+              </ButtonGroup>
+            </Grid>
+          </Grid>
+        }
+      />
+      {/* Other Routes */}
       <Route path="/join" element={<RoomJoinPage />} />
       <Route path="/create" element={<CreateRoomPage />} />
-      {/* home page route */}
-      <Route path="/" element={<p>Welcome to the Home Page!</p>} />
-      {/* Room when joined */}
       <Route path="/room/:roomCode" element={<Room />} />
-      {/* catch-all route */}
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
