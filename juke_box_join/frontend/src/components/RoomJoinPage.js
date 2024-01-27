2bce7a115605cba5e1885456fbc9a6664c307b47
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-01-27 14:18:44 -0700

    add room join page basic layout  basic console message on submit

--- juke_box_join/frontend/src/components/RoomJoinPage.js
+++ juke_box_join/frontend/src/components/RoomJoinPage.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { TextField, Button, Grid, Typography } from "@mui/material";
 
 const RoomJoinPage = () => {
-  return <h1>RoomJoinPage</h1>;
+  const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const _handleTextFieldChange = (e) => {
+    setRoomCode(e.target.value);
+  };
+
+  const _handleRoomJoin = () => {
+    console.log("Join room with code:", roomCode);
+    // Here, add logic to validate and join the room,
+    // and then navigate to the room page, if successful.
+    // navigate(`/room/${roomCode}`);
+  };
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} sx={{ textAlign: "center" }}>
+        <Typography component="h4" variant="h4">
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sx={{ textAlign: "center" }}>
+        <TextField
+          error={error !== ""}
+          label="Code"
+          placeholder="Enter a Room Code"
+          value={roomCode}
+          onChange={_handleTextFieldChange}
+          helperText={error}
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={12} sx={{ textAlign: "center" }}>
+        <Button variant="contained" color="primary" onClick={_handleRoomJoin}>
+          Join Room
+        </Button>
+      </Grid>
+      <Grid item xs={12} sx={{ textAlign: "center" }}>
+        <Button variant="contained" color="secondary" to='/' component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default RoomJoinPage;
