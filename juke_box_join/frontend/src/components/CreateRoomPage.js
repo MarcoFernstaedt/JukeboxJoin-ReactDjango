c65f947d4710843b427a9e3b06ed01c99de6027b
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-01-27 14:56:42 -0700

    refactor: imports

--- juke_box_join/frontend/src/components/CreateRoomPage.js
+++ juke_box_join/frontend/src/components/CreateRoomPage.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import FormHelperText from "@mui/material/FormHelperText";
-import FormControl from "@mui/material/FormControl";
 import { Link, useNavigate } from "react-router-dom";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
+import {
+  Button,
+  Grid,
+  Typography,
+  TextField,
+  FormHelperText,
+  FormControl,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+} from "@mui/material";
 
 const CreateRoomPage = () => {
   const [guestCanPause, setGuestCanPause] = useState(true);
   const defaultVotes = 2;
   const [votesToSkip, setVotesToSkip] = useState(defaultVotes);
-  const navagate = useNavigate();
+  const navigate = useNavigate();
 
   const _handleVotesChange = (e) => {
     setVotesToSkip(e.target.value);
@@ -43,7 +45,7 @@ const CreateRoomPage = () => {
         }
       })
       .then((data) => {
-        navagate(`/room/${data.code}`);
+        navigate(`/room/${data.code}`);
       })
       .catch((err) => {
         console.error(err);
