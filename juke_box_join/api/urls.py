c30aeede6a6bd5909212bf168fe87c2c69dc65d6
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-01-27 14:20:49 -0700

    add url path roomjoinview

--- juke_box_join/api/urls.py
+++ juke_box_join/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom
+from .views import RoomView, CreateRoomView, GetRoom, RoomJoinView
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
-    path('get-room', GetRoom.as_view())
+    path('get-room', GetRoom.as_view()),
+    path('join-room', RoomJoinView.as_view())
 ]
