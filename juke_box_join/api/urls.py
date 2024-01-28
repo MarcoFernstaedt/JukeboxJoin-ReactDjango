bf54dafe947522b0e1ae080b2a84372117cac5c9
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-01-27 17:24:42 -0700

    refactor: path uri now check-room

--- juke_box_join/api/urls.py
+++ juke_box_join/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, RoomJoinView, UserBelongsToRoom
+from .views import RoomView, CreateRoomView, GetRoom, RoomJoinView, CheckRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path('join-room', RoomJoinView.as_view()),
-    path('user-in-room', UserBelongsToRoom.as_view())
+    path('check-room', CheckRoom.as_view())
 ]
