62c9de4008cf445c61b361b3d808e571f4f7c808
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-01-27 19:11:36 -0700

    leaveroom view created remove room_code from session if user is host room deleted and returns to homepage

--- juke_box_join/api/views.py
+++ juke_box_join/api/views.py
@@ -80,3 +80,15 @@ class CheckRoom(APIView):
         data = {'code': self.request.session.get('room_code', None)}
 
         return JsonResponse(data, status=status.HTTP_200_OK)
+ 
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        if 'room_code' in self.request.session:
+            self.request.session.pop('room_code')
+            host_id = self.request.session.session_key
+            room = Room.objects.filter(host=host_id).first()
+            if room:
+                room.delete()
+                return Response({'message': 'Room deleted successfully'}, status=status.HTTP_200_OK)
+
+        return Response({'message': 'Success'}, status=status.HTTP_200_OK)
