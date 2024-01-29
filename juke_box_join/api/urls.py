d68e497ef762698f40f2d6a7034ccaf72f27a35a
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-01-29 10:14:41 -0700

    create uri path for update settings view

--- juke_box_join/api/urls.py
+++ juke_box_join/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, RoomJoinView, CheckRoom, LeaveRoom
+from .views import RoomView, CreateRoomView, GetRoom, RoomJoinView, CheckRoom, LeaveRoom, UpdateRoomSettingsView, IsUserHost
 
 urlpatterns = [
     path('room', RoomView.as_view()),
@@ -7,5 +7,7 @@ urlpatterns = [
     path('get-room', GetRoom.as_view()),
     path('join-room', RoomJoinView.as_view()),
     path('check-room', CheckRoom.as_view()),
-    path('leave-room', LeaveRoom.as_view())
+    path('leave-room', LeaveRoom.as_view()),
+    path('update-room', UpdateRoomSettingsView.as_view()),
+    path('is-host', IsUserHost.as_view())
 ]
